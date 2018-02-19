01a98e70ce54b557912f87d6b0fc0ff4a037d29f
nmohamed <jurukix@gmail.com>
2018-02-19 18:52:49 -0500

    ex 03 completed

---
+++ exercises/ex03/tee.c
@@ -0,0 +1,57 @@
+/*
+  Nora Mohamed
+  HW 03
+  Implemention of the "tee" command.
+  3) It took a while to read through the documentation for file IO. I started with trying to use fgets, but ran into difficult so I just tried fgetc and went with that instead. I'd like to try to get htat to work next time. Also, more error checking.
+  4) Uses malloc/better use of pointers. It also has no comments. Seems to care more about space allocation.
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  char c;
+  int opt;
+  int append = -1;
+
+  // Check command line arguments
+  while((opt = getopt(argc, argv, "a")) != -1){
+    switch(opt){
+      case 'a':
+        append = 0;
+        break;
+    }
+  }
+
+  // Open file to write to
+  if (append == 0) {
+    fp = fopen("./test.txt", "a");
+  }
+  else {
+    fp = fopen("./test.txt", "w+");
+  }
+  if(fp == NULL) {
+      perror("Error opening file");
+      return(-1);
+   }
+
+  // Read character from stdin while stdin still has characters to read from
+  while (!feof(stdin)) {
+    c = fgetc(stdin);
+
+    // Check for EOF character
+    if (c == EOF){
+      break;
+    }
+    printf("%c", c);
+
+    // Write to file
+    fputc(c, fp);
+  }
+
+  // Close file
+  fclose(fp);
+  return 0;
+}
