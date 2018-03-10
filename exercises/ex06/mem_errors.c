88649b19828508df4be317a5bcb48adabe5cf127
nmohamed <jurukix@gmail.com>
2018-03-10 17:18:13 -0500

    fin ex06

--- exercises/ex06/mem_errors.c
+++ exercises/ex06/mem_errors.c
@@ -15,13 +15,16 @@ void free_anything(int *p) {
 }
 
 int read_element(int *array, int index) {
-    int x = array[index];
-    return x;
+    if(index >= 0 && index <= sizeof(array)){
+      int x = array[index];
+      return x;
+    }
+    exit(0);
 }
 
 int main()
 {
-    int never_allocated;
+    int never_allocated = 1;
     int *free_twice = malloc(sizeof (int));
     int *use_after_free = malloc(sizeof (int));
     int *never_free = malloc(sizeof (int));
@@ -38,10 +41,11 @@ int main()
 
     // and it catches use after free
     free(use_after_free);
-    *use_after_free = 17;
+    use_after_free = malloc(sizeof (int));
 
     // never_free is definitely lost
     *never_free = 17;
+    free(never_free);
 
     // the following line would generate a warning
     // free(&never_allocated);
@@ -50,7 +54,7 @@ int main()
     free_anything(&never_allocated);
 
     free(free_twice);
-    free(free_twice);
+    // free(free_twice);
 
     return 0;
 }
